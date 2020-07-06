42d5ff4c861779a0669b4643589ee83686e93941
buckees <67809187+buckees@users.noreply.github.com>
2020-07-06 16:21:18 -0700

    Create 1dnorm_test.py
    Add test file for 1d-normal distribution

---
+++ 1dnorm_test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# This ES-PIC_run.py runs the PIC simulation
+# All in SI unit
+
+import Constants as cst
+import numpy as np
+from ESPIC1d_init import *
+from Particle import *
+import matplotlib.pyplot as plt
+
+# Geometry and Mesh
+length = 0.01 # lenght of domain, unit in m
+
+
+# Operation Conditions
+press_mT = 100.0 # pressure, unit in mTorr
+pressure = press_mT/1e3*133.3 # unit in Pa, 1 Torr = 133.322 Pa
+eon_temp = 2.0 # unit in eV
+Arp.temp = 100.1 # unit in eV
+
+# Model Parameters
+num_cell = 10 # number of cells
+num_ptcl = 1000 # number of particles
+time_step = 1e-12 # unit in s
+num_iter = 200 # number of iterations
+
+print(Arp.temp,Arp.mass)
+vels = norm_distribution(Arp.temp,Arp.mass,num_ptcl)
+
+fig, (ax0,ax1) = plt.subplots(2,1)
+ax0.hist(vels, density=True, histtype='stepfilled', alpha=0.5)
+ax0.legend(loc='best', frameon=False)
+
+ax1.plot(vels,range(len(vels)),'o')
+plt.show()
